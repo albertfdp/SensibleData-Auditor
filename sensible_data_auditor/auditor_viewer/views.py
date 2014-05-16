38ad4370c0cc131f7b64be2c82a3bd19915a12bf
Albert Fernández <albertfdp@gmail.com>
2014-05-16 17:51:24 +0200

    update

--- sensible_data_auditor/auditor_viewer/views.py
+++ sensible_data_auditor/auditor_viewer/views.py
@@ -1,15 +1,46 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django_sensible import oauth2
+from django_sensible import SECURE_CONFIG
+from django.conf import settings
+from django.core.urlresolvers import reverse
+
+def getTokens(request):
+	tokens = {}
+	t1 = oauth2.getToken(request.user, 'connector_raw.all_data')
+	#try to refresh token
+	r = oauth2.query(settings.SERVICE_URL+'connectors/connector_raw/v1/location/', t1, '&dummy=True&decrypted=True', SECURE_CONFIG.CLIENT_ID, SECURE_CONFIG.CLIENT_SECRET, settings.APPLICATION_URL[-1]+reverse('grant'), settings.SERVICE_REFRESH_TOKEN_URL)
+
+	t1 = oauth2.getToken(request.user, 'connector_raw.all_data')
+	if not t1 == None: tokens['connector_raw_all_data'] = t1
+
+	t2 = oauth2.getToken(request.user, 'connector_raw.all_data_researcher')
+	r = oauth2.query(settings.SERVICE_URL+'connectors/connector_raw/v1/location/', t2, '&dummy=True&decrypted=False', SECURE_CONFIG.CLIENT_ID, SECURE_CONFIG.CLIENT_SECRET, settings.APPLICATION_URL[-1]+reverse('grant'), settings.SERVICE_REFRESH_TOKEN_URL)
+
+	t2 = oauth2.getToken(request.user, 'connector_raw.all_data_researcher')
+	if not t2 == None: tokens['connector_raw_all_data_researcher'] = t2
+	return tokens
 
 def home(request):
     return render_to_response('home.html', {}, context_instance=RequestContext(request))
 
+@login_required
 def dashboard(request):
     return render_to_response('dashboard.html', {}, context_instance=RequestContext(request))
 
+@login_required
 def dashboard_researchers(request):
-    return render_to_response('researchers.html', {}, context_instance=RequestContext(request))
+	"""
+	tokens = getTokens(request)
+	scope = 'connector_raw.all_data=checked'
+
+	if not tokens:
+		return render_to_response('sensible/start_auth.html', {'scope': scope, 'dashboard_url': settings.SERVICE_URL+'researcher/'}, context_instance=RequestContext(request))
+	"""
+	tokens = {'bearer_token': 'aec2a27aa6ad3feb01eb2fe8c6ebd3'}
+	return render_to_response('researchers.html', {'tokens' : tokens}, context_instance=RequestContext(request))
 
 def dashboard_probes(request):
     return render_to_response('accesses.html', {}, context_instance=RequestContext(request))
--- sensible_data_auditor/static/js/researchers.js
+++ sensible_data_auditor/static/js/researchers.js
@@ -1,100 +1,257 @@
+DATA_USER_URL = 'http://raman.imm.dtu.dk:8081/albert/sensible-dtu/audit/researchers/user';
+DATA_RESEARCHERS_URL = 'http://raman.imm.dtu.dk:8081/albert/sensible-dtu/audit/researchers';
+
 queue()
-  .defer(d3.json, DATA_URL)
-  .defer(d3.json, DATA_RESEARCHERS_URL)
+  .defer(d3.json, DATA_USER_URL + '?bearer_token=' + SENSIBLE_BEARER_TOKEN)
+  .defer(d3.json, DATA_RESEARCHERS_URL + '?bearer_token=' + SENSIBLE_BEARER_TOKEN)
   .await(dashboard);
 
-var dimensions = {},
-    groups = {},
-    charts = {};
+var sensible = {};
+    sensible.dimensions = {},
+    sensible.groups = {},
+    sensible.charts = {},
+    sensible.composite = {};
 
-function dashboard(error, userData, aggData) {
+function dashboard(error, data, agg) {
+
+  if (error) return showErrorMessage(error);
+
+  console.log(data.result);
 
   sensible.ndx = crossfilter();
+  // parse the data
+  data.result.forEach(function(d) {
+    var formatter = d3.time.format("%Y-%j");
+    d.user = true;
+    d.date = new Date(formatter.parse(d.date.y + '-' + d.date.d));
+  });
+  agg.result.forEach(function(d) {
+    var formatter = d3.time.format("%Y-%j");
+    d.user = false;
+    d.date = new Date(formatter.parse(d.date.y + '-' + d.date.d));
+  });
 
-  // clean the data
-  userData.result.forEach(UserAccessesParser);
-  aggData.result.forEach(AggAccessessParser);
+  sensible.ndx.add(data.result);
+  sensible.ndx.add(agg.result);
 
-  // add them to crossfilter
-  sensible.ndx.add(userData.result);
-  sensible.ndx.add(aggData.result);
+  sensible.dimensions.date = sensible.ndx.dimension(function(d) {
+    return d.date;
+  });
 
-  dimensions.days = sensible.ndx.dimension(function(d) { return d.day;});
-  dimensions.researcher = sensible.ndx.dimension(function(d) {
+  sensible.dimensions.researcher = sensible.ndx.dimension(function(d) {
     return d.researcher;
   });
 
-  groups.dailyAccess = dimensions.days.group().reduceCount();
-
-  charts.researchers = {};
-  groups.researchers = {};
-  dimensions.researcher.group().all().forEach(function(researcher) {
-    if (researcher.key == "") return;
-
-
-    $('<div id="' + researcher.key + '" class="col-md-4"><div class="panel panel-default"><div class="panel-heading">' + researcher.key + '</div><div class="panel-body"><div id="accesses-' + researcher.key + '"></div></div></div></div>').appendTo('#researchers');
-    
-    groups.researchers[researcher.key] = dimensions.days.group().reduceCount();
-
-    charts.researchers[researcher.key] = dc.barChart('#accesses-' + researcher.key);
-    charts.researchers[researcher.key]
-      .height(200)
-      .width(400)
-      .x(d3.time.scale().domain([new Date(2014, 0, 1), new Date(2014, 31, 11)]))
-      .round(d3.time.day.round)
-      .margins({top: 10, right: 0, bottom: 30, left: 50})
-      .renderHorizontalGridLines(true)
-      .xUnits(d3.time.days)
-      .elasticY(true)
-      .renderTitle(true)
-      .yAxisLabel('Data points accessed')
-      .xAxisLabel('Week')
-      .brushOn(true)
-      .dimension(dimensions.days)
-      .group(groups.researchers[researcher.key]);
-
+  sensible.dimensions.probeSeries = sensible.ndx.dimension(function(d) {
+    return [d.date, d.probe];
   });
 
-  charts.accessesChart = dc.lineChart('#sensible-accesses-chart');
-  charts.accessesChart
-    .height(200)
-    .width(1200)
-    .renderArea(true)
-    .x(d3.time.scale().domain([new Date(2014, 0, 1), new Date(2014, 31, 11)]))
-    .round(d3.time.day.round)
+  sensible.dimensions.probe = sensible.ndx.dimension(function(d) {
+    return d.probe;
+  });
+
+  sensible.groups.accesses = sensible.dimensions.date.group().reduce(
+      reduceAdd, reduceRemove, reduceInit);
+
+  sensible.groups.accessPerResearcher = sensible.dimensions.researcher.group()
+    .reduce(reduceAdd, reduceRemove, reduceInit);
+
+  sensible.groups.probeHistogram = sensible.dimensions.probeSeries.group()
+    .reduce(reduceAdd, reduceRemove, reduceInit);
+
+  sensible.groups.probe = sensible.dimensions.probe.group()
+    .reduce(reduceAdd, reduceRemove, reduceInit);
+
+  var WIDTH = 1200;
+  var HEIGHT = 300;
+  var gap = 60, translate = 55;
+
+  sensible.dimensions.date = sensible.ndx.dimension(function(d) { return d3.time.month(d.date); });
+  sensible.groups.accesses = sensible.dimensions.date.group().reduce(reduceAdd, reduceRemove, reduceInit);
+  sensible.charts.composite = dc.compositeChart('#sensible-composite-chart');
+  sensible.charts.accessHistogram = dc.barChart('#sensible-accesses-chart');
+
+  sensible.composite.user = dc.barChart(sensible.charts.composite)
+    .gap(gap)
+    .group(sensible.groups.accesses, "User")
+    .valueAccessor(function (d) {
+      return d.value.userCount;
+    });
+  sensible.composite.average = dc.barChart(sensible.charts.composite)
+    .gap(gap)
+    .group(sensible.groups.accesses, "Average")
+    .valueAccessor(function (d) {
+      return (d.value.count / d.value.users);
+    })
+    .colors(d3.scale.ordinal().range(['#e34a33']));
+
+  sensible.charts.composite
+    .width(WIDTH)
+    .height(HEIGHT)
+    .transitionDuration(1000)
     .margins({top: 10, right: 0, bottom: 30, left: 50})
-    .renderHorizontalGridLines(true)
+    .dimension(sensible.dimensions.date)
+    .group(sensible.groups.accesses)
+    .elasticY(true)
+    .yAxisLabel('Data points accessed')
+    .rangeChart(sensible.charts.accessHistogram)
+	  //.x(d3.time.scale().domain(getDateRange(group)))
+    .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
+    .x(d3.time.scale().domain([new Date(2013, 6, 1), new Date(2014, 6, 31)]))
+	  .xUnits(d3.time.months)
+	  .round(d3.time.month.round)
+	  .renderHorizontalGridLines(true)
+    .compose([sensible.composite.user, sensible.composite.average])
+      .brushOn(false)
+     .renderlet(function (chart) {
+	       chart.selectAll("g._1").attr("transform", "translate(" + translate + ", 0)");
+      });
+
+
+
+
+  sensible.charts.accessHistogram
+    .height(60)
+    .width(1200)
+    .centerBar(true)
+    //.renderArea(true)
+    //.dashStyle([3,1,1,1])
+    .dimension(sensible.dimensions.date)
+    .group(sensible.groups.accesses)
+    .x(d3.time.scale().domain([new Date(2013, 6, 1), new Date(2014, 6, 31)]))
+    .margins({top: 10, right: 0, bottom: 30, left: 50})
     .xUnits(d3.time.days)
     .elasticY(true)
-    .renderTitle(true)
-    .yAxisLabel('Data points accessed')
-    .xAxisLabel('Week')
+    //.renderTitle(true)
+    //.legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
     .brushOn(true)
-    .dimension(dimensions.days)
-    .group(groups.dailyAccess, 'User accesses');
+    .valueAccessor(function(d) {
+      return d.value.userRequests;
+    }).yAxis().ticks(0);
+
+  sensible.charts.researchers = dc.rowChart('#sensible-researchers');
+  sensible.charts.researchers
+    .height(400)
+    .width(380)
+    .margins({top: 10, right: 10, bottom: 30, left: 0})
+    .dimension(sensible.dimensions.researcher)
+    .elasticX(true)
+    .valueAccessor(function(d) {
+      return d.value.requests;
+    })
+    .ordinalColors(colorbrewer.Spectral[9])
+    .group(sensible.groups.accessPerResearcher);
+
+  sensible.charts.probes = dc.rowChart('#sensible-probes');
+  sensible.charts.probes
+    .height(400)
+    .width(380)
+    .margins({top: 10, right: 10, bottom: 30, left: 0})
+    .dimension(sensible.dimensions.probe)
+    .elasticX(true)
+    .valueAccessor(function(d) {
+      return d.value.requests;
+    })
+    .ordinalColors(colorbrewer.Dark2[6])
+    .group(sensible.groups.probe);
+
+  sensible.charts.probes = dc.seriesChart('#sensible-probes-histogram');
+  sensible.charts.probes
+    .height(400)
+    .width(400)
+    .chart(function(c) {
+      return dc.lineChart(c)
+        .interpolate('basis')
+        .dashStyle([3,1,1,1])
+        .renderDataPoints({radius: 5, fillOpacity: 0.8, strokeOpacity: 0.8});
+      })
+    .seriesAccessor(function(d) {
+        return d.key[1];
+    })
+    .keyAccessor(function(d) {return d.key[0];})
+    .margins({top: 10, right: 10, bottom: 30, left: 20})
+    .dimension(sensible.dimensions.probeSeries)
+    .x(d3.time.scale().domain([new Date(2013, 6, 1), new Date(2014, 6, 31)]))
+    .xUnits(d3.time.days)
+    .elasticY(true)
+    .brushOn(false)
+    //.rangeChart(sensible.charts.accessHistogram)
+    .renderHorizontalGridLines(true)
+    .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5).horizontal(1).legendWidth(140).itemWidth(70))
+    .valueAccessor(function(d) {
+      return +d.value.requests;
+    })
+    .group(sensible.groups.probeHistogram);
+    sensible.charts.probes.yAxis().tickFormat(function(d) {
+        return d;
+    });
+  sensible.charts.probes.xAxis().tickFormat(d3.time.format("%b"));
+
+
+  $('#control-accesses').on('change', function(event) {
+    var target = event.target;
+    if (target.id == 'requests') {
+      Object.keys(sensible.charts).forEach(function(chart) {
+
+        if (chart == 'composite') {
+          sensible.composite.user
+            .valueAccessor(function(d) { return d.value.userRequests;});
+          sensible.composite.average
+            .valueAccessor(function(d) { return d.value.requests;});
+          sensible.charts[chart].compose([sensible.composite.user, sensible.composite.average]).render();
+        } else {
+          sensible.charts[chart].valueAccessor(function(d) { return d.value.requests;}).render();
+        }
+      });
+    } else {
+      Object.keys(sensible.charts).forEach(function(chart) {
+        if (chart == 'composite') {
+          sensible.composite.user
+            .valueAccessor(function(d) { return d.value.userCount;});
+          sensible.composite.average
+            .valueAccessor(function(d) { return d.value.count / d.value.users;});
+          sensible.charts[chart].compose([sensible.composite.user, sensible.composite.average]).render();
+        } else {
+          sensible.charts[chart].valueAccessor(function(d) { return d.value.count;}).render();
+        }
+      });
+    }
+  });
 
   dc.renderAll();
 
+
 }
 
-function UserAccessesParser(d) {
-  var formatter = d3.time.format("%Y-%m-%d");
-  d.user = true;
-  d.time = new Date(formatter.parse(d.date.y + "-" + d.date.m + "-" + d.date.d));
-  d.day = d3.time.day(d.time);
-  d.researcher = d.researcher;
-  d.probe = d.probe;
-  d.count = +d.count; // number of queries
-  d.dataAccesses = +d.data;
+function showErrorMessage(error) {
+  $('#audit-error').addClass('alert alert-danger')
+    .append('<p><strong>Error!</strong> Something went wrong.<p>')
+    .append('<p>' + error.status + ' ' + error.statusText + '</p>');
+  console.error(error);
 }
 
-function AggAccessessParser(d) {
-  var formatter = d3.time.format("%Y-%m-%d");
-  d.user = false;
-  d.time = new Date(formatter.parse(d.date.y + "-" + d.date.m + "-" + d.date.d));
-  d.day = d3.time.day(d.time);
-  d.researcher = d.researcher;
-  d.probe = d.probe;
-  d.count = +d.accessesCount; // number of queries
-  d.dataAccesses = +d.dataAccesses;
+function reduceAdd(p, v) {
+  if (v.user) {
+    p.userCount += v.dataCount;
+    p.userRequests += v.requestCount;
+  } else {
+    p.requests += v.requestCount;
+    p.count += v.dataCount;
+    p.users += v.users;
+  }
+  return p;
 }
+
+function reduceRemove(p, v) {
+  if (v.user) {
+    p.userCount -= v.dataCount;
+    p.userRequests -= v.requestCount;
+  } else {
+    p.requests -= v.requestCount;
+    p.count -= v.dataCount;
+    p.users -= v.users;
+  }
+  return p;
+}
+
+function reduceInit() { return {count : 0, requests: 0, users : 0, userCount : 0, userRequests : 0}};
